ccf423f0f5a22a566103ad07789291cfeb2dcd14
carol-palomo <85017737+carol-palomo@users.noreply.github.com>
2021-10-18 16:24:36 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 cd ./terraform/
-~/projetos/java-terraform-ansible/terraform/terraform init
-~/projetos/java-terraform-ansible/terraform/terraform validate
-~/projetos/java-terraform-ansible/terraform/terraform apply -auto-approve
+terraform init
+terraform validate
+terraform apply -auto-approve
 
 
 echo "Aguardando criação de maquinas ..."
