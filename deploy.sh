c64f7aea9e3808300d1587dc07adbb7e8f172768
carol-palomo <85017737+carol-palomo@users.noreply.github.com>
2021-10-18 15:48:48 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,19 @@ cd ../terraform
 ~/terraform/terraform apply -auto-approve
 
 echo "Aguardando criação de maquinas ..."
-sleep 10 # 10 segundos
+sleep 30 # 30 segundos
 
 echo $"[ec2-java]" > ../ansible/hosts # cria arquivo
 echo "$(~/terraform/terraform output | awk '{print $3;exit}')" >> ../ansible/hosts # captura output faz split de espaco e replace de ",
 
 echo "Aguardando criação de maquinas ..."
-sleep 10 # 20 segundos
+sleep 10 # 10 segundos
 
 cd ../ansible
 sudo ansible-playbook -i hosts playbook.yml -u ubuntu --private-key /root/.ssh/id_rsa
+
+echo "Aguardando execução do playbook ansible ..."
+sleep 30 # 10 segundos
 
 cd ../terraform
 
